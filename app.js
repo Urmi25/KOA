2441fc039affac84d95bce92cf561528f31d7436
Urmi25 <farjanaurmi25@gmail.com>
2024-04-03 23:26:45 +0600

    KOA

---
+++ app.js
@@ -0,0 +1,15 @@
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+app.use(async (ctx) => {
+  ctx.body = 'Hello KOA';
+});
+
+app.listen(3000, ()=> console.log('server is running'));
